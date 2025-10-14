9311e2d83ef6dfbc2b555087ccd90f31fa7f94ee
ZAKARIA <zakaria.lahrizi@insat.ucar.tn>
2025-10-14 15:36:30 +0100

    ajouter puissance

--- tp 2.1.py
+++ tp 2.1.py
@@ -12,15 +12,25 @@ def read_data_from_cmd ( ) :
     #print(out)
     return out
 
-def extract_puissance (out) :
+def extract_puissance (out,aff) :
     if platform.system() == 'Windows':
-        m = re.findall('SSID\s*:\s*([A-z0-9 _-]+).*?Signal.*?:.*?([0-9]+%)',out,re.DOTALL)
+        if (aff):
+            m = re.findall('SSID\s*:\s*([A-z0-9 _-]+).*?Signal.*?:.*?([0-9]+%)',out,re.DOTALL)
+        else:
+            m = re.findall('SSID\s*:\s*([A-Za-z0-9 _-]+).*?Rssi\s*:\s*(-?\d+)',out,re.DOTALL)
     else:
         raise Exception('reached else of if statement')
     return m
 
 
-print(read_data_from_cmd())
-while(True):
-    print(extract_puissance(read_data_from_cmd()))
-    time.sleep(1)
+bool=False
+while(bool==False):
+    aff=int(input("entrez le parametre que vous vouler afficher \n(0)la puissance du signal\n(1) le pourcetage:\n"))
+    if aff!=0 and aff!=1:
+        print("donner un entier valide (0 ou 1)")
+    else:
+        bool=True
+        print(read_data_from_cmd())
+        while(True):
+            print(extract_puissance(read_data_from_cmd(),aff))
+            time.sleep(1)
